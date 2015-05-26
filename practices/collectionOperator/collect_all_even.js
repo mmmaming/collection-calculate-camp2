e3d01596ff64e3adc2325751e25c4d9be92f50b2
ma ming <mmmmaming@outlook.com>
2015-05-26 19:43:23 +0800

    1

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,7 +1,19 @@
 'use strict';
 
+
 function collect_all_even(collection) {
-  //在这里写入代码
+ // var _ = require('../lodash/array');
+ // var a = _.remove(collection,function(n) {
+ //      return n % 2 == 0;
+ //  })
+ //  return a;
+ var array = [];
+ for(var i = 0;i < collection.length;i++) {
+     if(collection[i] % 2 == 0) {
+         array.push(collection[i]);
+     }
+ }
+ return array;
 }
 
 module.exports = collect_all_even;
