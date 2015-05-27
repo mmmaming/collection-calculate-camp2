17b63669d52851da6779f2c894d3bdd77cc3ef83
ma ming <mmmmaming@outlook.com>
2015-05-27 10:43:23 +0800

    override

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function choose_even(collection) {
-
-  //在这里写入代码
+    var array = [];
+    function even(){
+        for(var i = 0;i < collection.length;i++) {
+            if(collection[i] % 2 == 0) {
+                array.push(collection[i]);
+            }
+        }
+    }
+    even();
+    return array;
 }
 
 module.exports = choose_even;
