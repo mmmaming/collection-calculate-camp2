107693102233b23ec30531434404f35233877211
ma ming <mmmmaming@outlook.com>
2015-05-27 10:46:50 +0800

    override

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //在这里写入代码
+    var array = [];
+    for(var i = 0;i < collection.length;i++) {
+        if(collection[i] % 3 == 0) {
+            array.push(collection[i]);
+        }
+    }
+    return array;
 }
 
 module.exports = choose_multiples_of_three;
