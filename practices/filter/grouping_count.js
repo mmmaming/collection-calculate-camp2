2ac07b881d65883b473a4498169faf15c47ecf5d
ma ming <mmmmaming@outlook.com>
2015-05-27 18:53:01 +0800

    override

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function grouping_count(collection) {
+  //
+  // //在这里写入代码
+  // var _ = require('../lodash');
+  // return _.countBy(collection);
+  //collection.sort();
+  var temp = {};
 
-  //在这里写入代码
+  for(var i = 0;i < collection.length;i++) {
+      if(!! temp[collection[i]]) {
+          temp[collection[i]]++;
+      }else{
+          temp[collection[i]]=0;
+          temp[collection[i]]++;
+      }
+
+  }
+  return temp;
 }
 
 module.exports = grouping_count;
