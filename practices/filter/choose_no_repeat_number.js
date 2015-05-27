886baf996568f0b571d0673fbadbcaacc66064d3
ma ming <mmmmaming@outlook.com>
2015-05-27 12:26:40 +0800

    three method

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -3,6 +3,43 @@
 function choose_no_repeat_number(collection) {
 
   //在这里写入代码
+  // var _ = require('../lodash');
+  // return _.uniq(collection,true);
+  //========方法1==========
+  // var array = [];
+  // for(var i = 0;i < collection.length;i++) {
+  //     var exist = false;
+  //     for(var j = 0;j < array.length;j++) {
+  //         if(collection[i] == array[j]){
+  //             exist = true;
+  //             break;
+  //         }
+  //     }
+  //     if(!exist) {
+  //         array.push(collection[i]);
+  //     }
+  // }
+  // return array;
+  //==========方法2=================
+//   collection.sort();
+//   var array = [];
+//   for(var i = 0;i < collection.length;i++){
+//       if(collection[i] == collection[i+1]){
+//           continue;
+//       }
+//       array.push(collection[i]);
+//   }
+//   return array;
+// }
+//=======方法3============
+    var array = [];
+    var hash = {};
+    for(var i = 0;collection[i] != null;i++) {
+        if(!hash[collection[i]]) {
+            array.push(collection[i]);
+            hash[collection[i]] = true;
+        }
+    }
+    return array;
 }
-
 module.exports = choose_no_repeat_number;
