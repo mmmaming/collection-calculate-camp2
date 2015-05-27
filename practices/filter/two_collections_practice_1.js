db808412edd7d45c6508cc03546db9ae5bc1c556
ma ming <mmmmaming@outlook.com>
2015-05-27 17:06:42 +0800

    override

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+    var array = [];
+    for(var i = 0;i < collection_a.length;i++) {
+        for(var j = 0;j < collection_b.length;j++) {
+            if(collection_a[i] == collection_b[j]) {
+                array.push(collection_a[i]);
+            }
+        }
+    }
+    return array;
 }
 
 module.exports = choose_common_elements;
