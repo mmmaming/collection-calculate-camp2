a0f7af6fdec73dd934f6d59ba58720d8f4261ac6
ma ming <mmmmaming@outlook.com>
2015-05-28 13:19:22 +0800

    override

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+    var array = [];
+    for(var i = 0;i < collection.length;i++) {
+    //  分别用长度和类型两种方法进行判断
+    //  if(collection[i].length!=undefined) {
+        if(typeof(collection[i])=='object') {
+            for(var j = 0;j < collection[i].length;j++) {
+                array.push(collection[i][j])
+           }
+        }else{
+            array.push(collection[i])
+        }
+    }
+    return array;
 }
 
 module.exports = double_to_one;
