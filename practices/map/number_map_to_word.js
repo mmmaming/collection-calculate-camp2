762901176392745f1f6c6d6fa9ed6d8d461ff90e
ma ming <mmmmaming@outlook.com>
2015-05-28 13:36:47 +0800

    override

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,17 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  // return ['a','b','c','d','e'];
+  //var _ = require('../lodash');
+  var array = [];
+  function a(collection_a){
+      for(var i=_.first(collection_a);i<=_.last(collection_a);i++) {
+          array.push( (String.fromCharCode(i+96)));
+      }
+  return array;
+  }
+
+
+
 };
 
 module.exports = number_map_to_word;
