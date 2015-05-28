58b4502007e504965e59bb96b64d7f00d746bda7
ma ming <mmmmaming@outlook.com>
2015-05-28 20:16:50 +0800

    nothing

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -2,6 +2,13 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  var max = 0;
+  for(var i = 0;i < collection.length;i++) {
+      if(collection[max] < collection[i]) {
+          max = i;
+      }
+  }
+  return collection[max];
 }
 
 module.exports = collect_max_number;
--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -2,7 +2,13 @@
 
 function collect_min_number(collection) {
   //在这里写入代码
+  var min = 0;
+  for(var i = 0;i < collection.length;i++) {
+      if(collection[min] > collection[i]) {
+          min = i;
+      }
+  }
+  return collection[min];
 }
 
 module.exports = collect_min_number;
-
