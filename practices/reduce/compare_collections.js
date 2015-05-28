dc2debfabc4e950d160a18d767218a7cd34032d7
ma ming <mmmmaming@outlook.com>
2015-05-28 20:39:46 +0800

    same and sum

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -2,8 +2,13 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  for(var i = 0;i < collection_a.length;i++) {
+      if(collection_a[i] == collection_b[i]){
+          return true;
+      }else{
+          return false;
+      }
+  }
 }
 
 module.exports = compare_collections;
-
-
