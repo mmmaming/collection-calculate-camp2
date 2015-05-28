191201eca3e12407b228bca9b70dda43d92e71c6
ma ming <mmmmaming@outlook.com>
2015-05-28 17:52:05 +0800

    nothing

--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,25 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
+
+    var map = [];
+    for(var i =0;collection[i]!=null;i++) {
+        var exist = false;
+        for(var j = 0;j < map.length;j++) {
+            if(collection[i] < map[j]){
+                map.splice(j,0,collection[i]);
+                exist = true;
+                break;
+            }
+
+        } if(!exist) {
+        map.push(collection[i]);
+          }
+    }
+    return map;
+
+
+
+
 };
 
 module.exports = rank_desc;
