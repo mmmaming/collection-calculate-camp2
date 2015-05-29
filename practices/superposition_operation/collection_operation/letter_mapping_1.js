bfb9103684b6c4d7cbc168e22f3afc3d7e1595b3
ma ming <mmmmaming@outlook.com>
2015-05-29 13:15:00 +0800

    mapping

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -3,6 +3,13 @@
 function even_to_letter(collection) {
 
   //在这里写入代码
+  var map = [];
+  for(var i = 0;i < collection.length;i++) {
+      if(collection[i] % 2 == 0) {
+          map.push(String.fromCharCode(collection[i]+96));
+      }
+  }
+  return map;
 }
 
 module.exports = even_to_letter;
--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -2,8 +2,12 @@
 
 function average_to_letter(collection) {
 
-  //在这里写入代码
+  //在这里计算，它返回的是大于或等于函数参数，并且与之最接近的整数。写入代码
+  var sum_avg = 0;
+  for(var i = 0;i < collection.length;i++) {
+      sum_avg += collection[i];
+  }
+  return String.fromCharCode(Math.ceil(sum_avg/collection.length)+96);
 }
 
 module.exports = average_to_letter;
-
