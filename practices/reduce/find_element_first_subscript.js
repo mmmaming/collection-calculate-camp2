3d25bac04763637a291ea78be42150d6859c0431
ma ming <mmmmaming@outlook.com>
2015-05-29 13:01:03 +0800

    find

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -2,7 +2,7 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  return collection[element];
 }
 
 module.exports = calculate_elements_sum;
-
--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,7 +2,11 @@
 
 function find_first_even(collection) {
   //在这里写入代码
+  for(var i = 0;i < collection.length;i++) {
+      if(collection[i] % 2 == 0) {
+          return collection[i];
+      }
+  }
 }
 
 module.exports = find_first_even;
-
