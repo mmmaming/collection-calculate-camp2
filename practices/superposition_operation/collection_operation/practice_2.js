6991bfee532d06913f23502236b1134246ffec5f
ma ming <mmmmaming@outlook.com>
2015-05-29 13:24:06 +0800

    operation

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -3,7 +3,13 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var array = [];
+  for(var i = 0;i < collection.length;i++) {
+      if(collection[i] % 2 != 0) {
+          array.push(collection[i]*3+2);
+      }
+  }
+  return array;
 }
 
 module.exports = hybrid_operation_to_uneven;
-
--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -3,7 +3,13 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
+  var sum = 0;
+  for(var i = 0;i < collection.length;i++) {
+      if(collection[i] % 2 != 0) {
+          sum += (collection[i] * 3 + 5);
+      }
+  }
+  return sum;
 }
 
 module.exports = hybrid_operation_to_uneven;
-
