3847a30b39379c59553d685bcbe3c1b9c2b4d97a
ma ming <mmmmaming@outlook.com>
2015-06-03 20:47:41 +0800

    first

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -2,11 +2,17 @@
 
 function find_first_even(collection) {
   //在这里写入代码
-  for(var i = 0;i < collection.length;i++) {
-      if(collection[i] % 2 == 0) {
-          return collection[i];
-      }
-  }
+  // for(var i = 0;i < collection.length;i++) {
+  //     if(collection[i] % 2 == 0) {
+  //         return collection[i];
+  //     }
+  // }
+  var _ = require('../mylodash/array.js')
+return _.first(collection,function(val) {
+    return val % 2 == 0;
+})
+
 }
+
 
 module.exports = find_first_even;
