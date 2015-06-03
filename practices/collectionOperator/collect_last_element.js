f618500543971bdeafa0449216f4353d41e787e5
ma ming <mmmmaming@outlook.com>
2015-06-03 20:34:38 +0800

    two method

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -6,14 +6,41 @@ function collect_last_element(collection) {
   // var last = _.last(collection);
   // return last;
 
-
   // var collection_length = collection.length;
   // return collection[collection_length-1];
-  var _ = require('../mylodash/array.js');
-  return _.reduce(collection,function(a,b) {
-      return b;
-  })
+  // var _ = require('../mylodash/array.js');
+  // return _.reduce(collection,function(a,b) {
+  //     return b;
+  // });
+  //
+
+  // var _ = require('../mylodash/array.js')
+  // return _.last(collection);
+  var a = last1(collection,function(n) {
+      return n % 2 ==0;
+  });
+  return a;
 
 }
+
+function last(array,fun) {
+    if(fun != undefined) {
+        array = filter(array,fun);
+    }
+    return reduce(array,function(a, b) {
+        return b;
+    })
+}
+
+function last1(array,fun) {
+    for(var i = array.length-1;i >= 0; i--) {
+        if(fun(array[i])){
+            return array[i];
+        }
+    }
+}
+
+
+
 
 module.exports = collect_last_element;
