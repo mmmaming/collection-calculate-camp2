d640bd0f36e68213470170e7d391e9bb7777779e
ma ming <mmmmaming@outlook.com>
2015-06-03 11:54:59 +0800

    mylodash

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -6,18 +6,34 @@ function grouping_count(collection) {
   // var _ = require('../lodash');
   // return _.countBy(collection);
   //collection.sort();
+  // var temp = {};
+  //
+  // for(var i = 0;i < collection.length;i++) {
+  //   //   temp[collection[i]]=temp[collection[i]]||0;
+  //   //   temp[collection[i]]++;
+  //     if(!! temp[collection[i]]) {
+  //         temp[collection[i]]++;
+  //     }else{
+  //         temp[collection[i]]=0;
+  //         temp[collection[i]]++;
+  //     }
+  //
+  // }
+  // return temp;
+  var _ = require('../mylodash/array.js');
+  var even_array = _.filter(collection,function(val) {
+      return val % 2 == 0;
+  });
   var temp = {};
+  _.each(even_array,function(val) {
+      var key = val;
+      var count = 1;
+      temp[key] = temp[key] || count;
+      temp[key] = count++;
 
-  for(var i = 0;i < collection.length;i++) {
-      if(!! temp[collection[i]]) {
-          temp[collection[i]]++;
-      }else{
-          temp[collection[i]]=0;
-          temp[collection[i]]++;
-      }
-
-  }
+  });
   return temp;
+
 }
 
 module.exports = grouping_count;
