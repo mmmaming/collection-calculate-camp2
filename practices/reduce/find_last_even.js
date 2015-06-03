064801e6457158e12385d5ef7924cfe51d85b35c
ma ming <mmmmaming@outlook.com>
2015-06-03 20:41:50 +0800

    last number

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,26 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  //=========方法1============
+  // for(var i = collection.length-1; i>= 0;i--) {
+  //     if(collection[i] % 2 == 0) {
+  //         return collection[i];
+  //     }
+  // }
+  //=========方法2===========
+  // var array = [];
+  // for(var i = 0;i< collection.length;i++) {
+  //     if(collection[i] % 2 == 0) {
+  //         array.push(collection[i]);
+  //
+  //     }
+  // }
+  // return array[array.length-1];
+  var _ = require('../mylodash/array.js');
+  return _.last(collection,function(val) {
+      return val % 2 == 0;
+  })
 }
+
 
 module.exports = find_last_even;
