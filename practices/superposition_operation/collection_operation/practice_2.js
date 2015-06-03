321c47c5ebb0e0b1d8d547d7a4ca1dadd4bd2430
ma ming <mmmmaming@outlook.com>
2015-06-03 13:30:24 +0800

    mylodash

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -4,12 +4,21 @@ function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
   var array = [];
-  for(var i = 0;i < collection.length;i++) {
-      if(collection[i] % 2 != 0) {
-          array.push(collection[i]*3+2);
+  // for(var i = 0;i < collection.length;i++) {
+  //     if(collection[i] % 2 != 0) {
+  //         array.push(collection[i]*3+2);
+  //     }
+  // }
+  // return array;
+  var _ = require('../../mylodash/array.js');
+  var odd = _.filter(collection,function(val) {
+      if(val % 2 != 0) {
+          return val;
       }
-  }
-  return array;
+  });
+  return _.map(odd,function (val) {
+      return val * 3 + 2;
+  })
 }
 
 module.exports = hybrid_operation_to_uneven;
--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -3,13 +3,24 @@
 function hybrid_operation_to_uneven(collection) {
 
   //在这里写入代码
-  var sum = 0;
-  for(var i = 0;i < collection.length;i++) {
-      if(collection[i] % 2 != 0) {
-          sum += (collection[i] * 3 + 5);
-      }
-  }
-  return sum;
+//   var sum = 0;
+//   for(var i = 0;i < collection.length;i++) {
+//       if(collection[i] % 2 != 0) {
+//           sum += (collection[i] * 3 + 5);
+//       }
+//   }
+//   return sum;
+// }
+    var _ = require('../../mylodash/array.js');
+    var odd = _.filter(collection,function(val) {
+        return val % 2 != 0;
+    });
+    var math = _.map(odd,function(val) {
+        return val * 3 + 5;
+    });
+    return _.reduce(math,function(a, b) {
+        return a + b;
+    });
 }
 
 module.exports = hybrid_operation_to_uneven;
