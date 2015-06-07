aa0c30d625ac0f5fb28be14aa8d1186e13ffce2a
ma ming <mmmmaming@outlook.com>
2015-06-07 17:42:03 +0800

    override

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,19 +1,32 @@
 'use strict';
 
 function double_to_one(collection) {
+    var _ = require('../mylodash/array.js');
     var array = [];
-    for(var i = 0;i < collection.length;i++) {
-    //  分别用长度和类型两种方法进行判断
-    //  if(collection[i].length!=undefined) {
-        if(typeof(collection[i])=='object') {
-            for(var j = 0;j < collection[i].length;j++) {
-                array.push(collection[i][j])
-           }
+    _.each(collection,function(val,key) {
+        if(typeof(val) == 'object') {
+            _.each(val,function(n) {
+                array.push(n);
+            })
         }else{
-            array.push(collection[i])
+            array.push(val);
         }
-    }
+    })
     return array;
+    // var array = [];
+    // for(var i = 0;i < collection.length;i++) {
+    // //  分别用长度和类型两种方法进行判断
+    // //  if(collection[i].length!=undefined) {
+    //     if(typeof(collection[i])=='object') {
+    //         for(var j = 0;j < collection[i].length;j++) {
+    //             array.push(collection[i][j])
+    //        }
+    //     }else{
+    //         array.push(collection[i])
+    //     }
+    // }
+    // return array;
+
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,25 +1,45 @@
 'use strict';
 
 function double_to_one(collection) {
+
+    var _ = require('../mylodash/array.js');
     var array = [];
-    for(var i = 0;i < collection.length;i++) {
-        if(typeof(collection[i])=='object') {
-            for(var j = 0;j < collection[i].length;j++) {
-                array.push(collection[i][j])
-           }
+    _.each(collection,function(val,key) {
+        if(typeof(val) == 'object') {
+            _.each(val,function(n) {
+                array.push(n);
+            })
         }else{
-            array.push(collection[i])
+            array.push(val);
         }
-    }
+    })
     var no_repeat_array = [];
-    var hash = {};
-    for(var i = 0;array[i] != null;i++) {
-        if(!hash[array[i]]) {
-            no_repeat_array.push(array[i]);
-            hash[array[i]] = true;
+    _.each(array,function(n) {
+        if(!_.exist(n,no_repeat_array)){
+            no_repeat_array.push(n);
         }
-    }
+    });
     return no_repeat_array;
+
+    // var array = [];
+    // for(var i = 0;i < collection.length;i++) {
+    //     if(typeof(collection[i])=='object') {
+    //         for(var j = 0;j < collection[i].length;j++) {
+    //             array.push(collection[i][j])
+    //        }
+    //     }else{
+    //         array.push(collection[i])
+    //     }
+    // }
+    // var no_repeat_array = [];
+    // var hash = {};
+    // for(var i = 0;array[i] != null;i++) {
+    //     if(!hash[array[i]]) {
+    //         no_repeat_array.push(array[i]);
+    //         hash[array[i]] = true;
+    //     }
+    // }
+    // return no_repeat_array;
 }
 
 module.exports = double_to_one;
