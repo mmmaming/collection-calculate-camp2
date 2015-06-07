8999735bd766381ef4ac2c3bcf72d1da021f0e24
ma ming <mmmmaming@outlook.com>
2015-06-07 19:27:22 +0800

    override

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,22 @@
 'use strict';
 
 function amount_even(collection) {
-
+    var _ = require('../../mylodash/array.js');
+    var even = _.filter(collection,function(n) {
+        return n % 2 === 0;
+    });
+    return _.reduce(even,function(a, b) {
+        return a + b;
+    })
   //在这里写入代码
+  // var sum = 0;
+  // for(var i = 0;i < collection.length;i++) {
+  //     if(collection[i] % 2 == 0) {
+  //         sum += collection[i];
+  //     }
+  // }
+  // return sum;
+
 }
 
 module.exports = amount_even;
--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,24 @@
 'use strict';
 
 function average_uneven(collection) {
-
+    var _ = require('../../mylodash/array.js');
+    var odd = _.filter(collection,function(n) {
+        return n % 2 != 0;
+    });
+     var sum = _.reduce(odd,function(a, b) {
+        return a + b;
+    })
+    return sum/odd.length;
   //在这里写入代码
+  // var avg = 0;
+  // var count = 0;
+  // for(var i = 0;i < collection.length;i++) {
+  //     if(collection[i] % 2 != 0) {
+  //         avg += collection[i];
+  //         count++;
+  //     }
+  // }
+  // return avg/count;
 }
 
 module.exports = average_uneven;
