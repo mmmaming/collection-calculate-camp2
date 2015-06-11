fc279e4afb089dbaa2e96ca90c6ef7238cda7f92
ma ming <mmmmaming@outlook.com>
2015-06-11 14:52:32 +0800

    array range

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,20 +1,8 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-    var _ = require('../mylodash/array.js');
-    return _.range(number_a, number_b);
-
-   // var array = [];
-   // if(number_a <= number_b) {
-   //     for(var i=number_a; i<=number_b; i++) {
-   //         array.push(i);
-   //     }
-   // }else {
-   //     for(var j=number_a; j>=number_b; j--) {
-   //         array.push(j);
-   //     }
-   // }
-   // return array;
+    var _ = require('../mylodash/object.js');
+    return _().range(number_a,number_b);
 
 }
 
