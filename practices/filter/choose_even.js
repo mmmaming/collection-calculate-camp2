592d4ddb453c28bd835234d811bbb098e35d6502
ma ming <mmmmaming@outlook.com>
2015-06-11 18:11:42 +0800

    choose even from array

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,66 +1,10 @@
 'use strict';
 
 function choose_even(collection) {
-    var _ = require('../mylodash/array.js');
-    return _.filter(collection, function(n) {
+    var _ = require('../mylodash/object.js');
+    return _(collection).filter(function(n) {
         return n % 2 === 0;
-    });
-
-    // var array = [];
-    // function even(){
-    //     for(var i = 0;i < collection.length;i++) {
-    //         if(collection[i] % 2 == 0) {
-    //             array.push(collection[i]);
-    //         }
-    //     }
-    // }
-    // even();
-    // return array;
-    // ----=-=-=-=================================
-    // function foreach(array,fun){
-    //     for(var i = 0;i < array.length;i++) {
-    //         fun(array[i],i);
-    //     }
-    // }
-    // function filter(collection,fun) {
-    //     var result = [];
-    //     foreach(collection,function(q) {
-    //             if(fun(q)){
-    //                 result.push(q);
-    //             }
-    //     });
-    //     return result;
-    // }
-    //
-    // return filter(collection,function(n) {
-    //
-    //     return n % 2 == 0;
-    // });
-  //------------------------------------------
-  //   function foreach(array,fun){
-  //      for(var i = 0;i < array.length;i++) {
-  //          fun(array[i],i);
-  //      }
-  //  }
-  //  function remove(collection,fun) {
-  //      var result = [];
-  //      foreach(collection,function(q) {
-  //              if(!fun(q)){
-  //                  result.push(q);
-  //              }
-  //      });
-  //      return result;
-  //  }
-  //
-  //  return remove(collection,function(n) {
-  //     if (n % 2 == 0) {
-  //         return true;
-  //     }
-  // });
-
-
-
-
+    }).value();
 
 }
 
