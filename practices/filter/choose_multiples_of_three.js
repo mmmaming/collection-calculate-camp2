e65787308010e2a6a304a06ddbd5224f12af9bf3
ma ming <mmmmaming@outlook.com>
2015-06-14 20:06:42 +0800

    choose multiples of three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,10 +1,39 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-    var _ = require('../mylodash/array.js');
-    return _.filter(collection,function(n) {
-        return n % 3 == 0;
-    })
+    return _(collection).filter(function(val, key) {
+        return val % 3 === 0;
+    }).value();
+
 }
+
+function _(collection) {
+    if(!( this  instanceof _)){
+        return new _(collection);
+    }
+    this.collection = collection;
+}
+
+_.prototype.each = function(fun){
+    for(var i = 0; i < this.collection.length; i++) {
+        fun(this.collection[i],i)
+    }
+}
+_.prototype.filter = function(fun) {
+    var result = [];
+    this.each(function(val, key){
+        if(fun(val, key)) {
+            result.push(val);
+        }
+    })
+    this.collection = result;
+    return this;
+}
+_.prototype.value = function(){
+    return this.collection;
+}
+
+
+
 
 module.exports = choose_multiples_of_three;
