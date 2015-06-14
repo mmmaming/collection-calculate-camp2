6bd6fecf906b394f8c54c0913d6b690f9f1d7120
ma ming <mmmmaming@outlook.com>
2015-06-14 21:14:40 +0800

    choose same element

--- practices/filter/two_collections_practice_1.js
+++ practices/filter/two_collections_practice_1.js
@@ -1,14 +1,14 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-    var _ = require('../mylodash/array.js');
-    var result = [];
-    _.each(collection_a,function(n) {
-        if(_.exist(n,collection_b)){
-            result.push(n);
-        }
-    })
-    return result;
+    // var _ = require('../mylodash/array.js');
+    // var result = [];
+    // _.each(collection_a,function(n) {
+    //     if(_.exist(n,collection_b)){
+    //         result.push(n);
+    //     }
+    // })
+    // return result;
 
   // //在这里写入代码
   // var _ = require('../lodash');
@@ -23,6 +23,37 @@ function choose_common_elements(collection_a, collection_b) {
     //     }
     // }
     // return array;
+    var result = [];
+    _(collection_a).each(function(n) {
+        if(_(collection_b).exist(n)) {
+            result.push(n)
+        }
+    })
+    return result;  
 }
+function _(collection) {
+    if(!(this instanceof _)){
+        return new _(collection);
+    }
+    this.collection = collection;
+}
+
+_.prototype.each = function(fun) {
+    for(var i = 0;i < this.collection.length;i++) {
+        fun(this.collection[i],i)
+    }
+}
+
+_.prototype.exist = function(element) {
+    var result = false;
+    this.each(function(val) {
+        if( val === element) {
+            result = true;
+        }
+    })
+    return result;
+}
+
+
 
 module.exports = choose_common_elements;
