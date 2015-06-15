16386cb49826dd53c71d29d01f5e7578530bb3c0
ma ming <mmmmaming@outlook.com>
2015-06-15 16:32:48 +0800

    choose different elements between two collections

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,21 +1,37 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-    var _ = require('../mylodash/array.js');
     var result = [];
-    _.each(collection_a,function(n) {
-        if(!_.exist(n,collection_b)){
-            result.push(n);
+    _(collection_a).each(function(val) {
+        if(!_(collection_b).exist(val)) {
+            result.push(val);
+        }
+    });
+    return result;
+}
+
+function _(collection) {
+    if(!(this instanceof _)) {
+        return  new _(collection);
+    }
+    this.collection = collection;
+}
+
+_.prototype.each = function(func) {
+    for(var i = 0; i < this.collection.length; i++) {
+        func(this.collection[i], i);
+    }
+    return this;
+}
+
+_.prototype.exist = function(element) {
+    var result = false;
+    this.each(function(val, key) {
+        if(val === element) {
+            result = true;
         }
     })
     return result;
-
-
-
-//   //在这里写入代码
-//   var _ = require('../lodash');
-// return _.xor(collection_a,collection_b)
-
 }
 
 module.exports = choose_no_common_elements;
